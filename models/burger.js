0518707ce2ee0519f9279b8b72d6117937819638
Brian Tom <briantom171@gmail.com>
2016-10-11 23:36:03 -0700

    everything but password_hash

---
+++ models/burger.js
@@ -0,0 +1,34 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define("Burger", {
+    burger_name: DataTypes.STRING,
+    devoured: { type: DataTypes.BOOLEAN, defaultValue: false }
+  }, {
+
+    underscored: true,
+
+    freezeTableName: true,
+
+    // define the table's name
+    tableName: 'burgers',
+
+    classMethods: {
+      associate: function(models) {
+        Burger.belongsTo(models.User, {
+          onDelete: "CASCADE",
+          foreignKey: {
+            allowNull: false
+          }
+        })
+      }
+    }
+  });
+
+  return Burger;
+};
+
+
+
+
+
